7d86d56983a2b0da68f52a977eb31bce74034a4a
Pedro Costa <scosta.pedro.manoel@gmail.com>
2024-06-08 22:18:55 -0300

    docs: move stories dist to /docs

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('@ladle/react').UserConfig} */
 export default {
   stories: 'examples/**/*.stories.{js,jsx,ts,tsx,mdx}',
-  // outDir: '.ladle/dist', // GitHub doesn't allow you to use non-root folders for some reason :(
+  outDir: 'docs', // GitHub doesn't allow you to use non-root folders for some reason :(
 };
